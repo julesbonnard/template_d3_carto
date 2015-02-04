4559718d4932eb8817b939ffddb7ee967e9a16e9
Jules Bonnard <julesbonnard@gmail.com>
2015-02-04 01:36:43 +0100

    Simple map is working git add .

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # template_d3_carto
 An empty template to project maps with topojson
+
+Extensive use of the tutoriel Let's Make a Map by Mike Bostock http://bost.ocks.org/mike/map/
--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-d3.json(topojson_url, function(error, topojson) {
-  if (error) return console.error(error);
-  console.log(topojson);
+//On détecte la taille du div
+var width = document.getElementById('map').offsetWidth,
+	height = document.getElementById('map').offsetHeight;
+
+//On déplace la projection au milieu
+var projection = projection.translate([width / 2, height / 2]);
+
+//On crée un svg de la taille du div
+var svg = d3.select("#map").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+var path = d3.geo.path()
+	.projection(projection);
+
+d3.json(topojson_url, function(error, topojson_object) {
+	if (error) return console.error(error);
+	console.log(topojson_object);
+
+	var subunits = topojson.feature(topojson_object, topojson_object.objects.subunits); //Reconvertir vers GeoJson
+
+	//Création des éléments
+	svg.selectAll(".subunit")
+		.data(topojson.feature(topojson_object, topojson_object.objects.subunits).features)
+		.enter().append("path")
+		.attr("class", function(d) {
+			return "subunit " + d.properties.name;
+		})
+		.attr("d", path);
+
+	//Création des bordures
+	svg.append("path")
+		.datum(topojson.mesh(topojson_object, topojson_object.objects.subunits, function(a, b) {
+			return a !== b;
+		}))
+		.attr("d", path)
+		.attr("class", "subunit-boundary");
 });
