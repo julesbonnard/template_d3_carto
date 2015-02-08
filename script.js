3c1755716368bc11c4bcd8a274763d67e2b9176e
Jules Bonnard <julesbonnard@gmail.com>
2015-02-09 00:34:45 +0100

    working

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var easyMap = function(cssSelector,urlTopojson) {
-	//On déplace la projection au milieu
-	this.projection = this.projection.translate([this.size/2,this.height/2]);
 	//Modification du css Selector
 	if(cssSelector !== undefined)
 		this.cssSelector = cssSelector;
 	//Modification de l'url Topojson
 	if(urlTopojson !== undefined)
 		this.urlTopojson = urlTopojson;
+
+	this.detectSize();
+
+	this.svg = d3.select(this.cssSelector).append('svg')
+			.attr('width',this.size.width)
+			.attr('height',this.size.height);
 };
 //Initialisation d'une taille standard
 easyMap.prototype.size = {
@@ -15,9 +19,10 @@ easyMap.prototype.size = {
 };
 //On détecte la taille du div
 easyMap.prototype.detectSize = function() {
-	this.size.width = d3.select(this.cssSelector).offsetWidth;
-	this.size.height = d3.select(this.cssSelector).offsetHeight;
+	this.size.width = d3.select(this.cssSelector)[0][0].offsetWidth;
+	this.size.height = d3.select(this.cssSelector)[0][0].offsetHeight;
 }
+
 //Initialisation d'une projection standard
 easyMap.prototype.projection = d3.geo.mercator();
 	
@@ -29,19 +34,14 @@ easyMap.prototype.urlTopojson = 'data/world-110m.json';
 
 //Rendu de la map
 easyMap.prototype.draw = function() {
-	console.log(this.svg)
+	var svg = this.svg;
 	//Initialisation du path
-	var path = d3.geo.path().projection(this.projection);
+	var path = d3.geo.path()
+			.projection(this.projection);
 
 	function ready(error,topojsonObject) {
-		//Ajout d'un svg de la taille indiquée
-		var svg = d3.select(this.cssSelector).append('svg')
-				.attr('width',this.size.width)
-				.attr('height',this.size.height);
-		//Convertir vers GeoJson
-		var subunits = topojson.feature(topojsonObject,topojsonObject.objects.subunits);
 		//Création des éléments
-		svg = selectAll('.subunit')
+		svg = svg.selectAll('.subunit')
 					.data(topojson.feature(topojsonObject,topojsonObject.objects.subunits).features)
 					.enter().append("path")
 					.attr("class",function(d) {
